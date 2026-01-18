3776bdb4fb36057959877437fd3c6790738ae00c
bruno.cupido@instride.com <Pic-Pay2023*>
2026-01-18 20:33:25 -0300

    Initial commit: React app and Terraform infrastructure

--- codebase/rdicidr-0.1.0/terraform/main.tf
+++ codebase/rdicidr-0.1.0/terraform/main.tf
@@ -1,9 +1,118 @@
 # In this file put all the logic to crete the proper infraestructure
 terraform {
     required_providers {
-        # Add the provideres according to the challenges
+            aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
     }
 }
 
+provider "aws" {
+  region = var.region
+}
 
-# Add the resources relatedo to the provider
+# --------------------
+# S3 bucket
+# --------------------
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = var.environment
+    Project     = "rdicidr"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "site" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.site,
+    aws_s3_bucket_public_access_block.site
+  ]
+
+  bucket = aws_s3_bucket.site.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# --------------------
+# CloudFront
+# --------------------
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for rdicidr static site"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Environment = var.environment
+    Project     = "rdicidr"
+  }
+}
+
--- codebase/rdicidr-0.1.0/terraform/variables.tf
+++ codebase/rdicidr-0.1.0/terraform/variables.tf
@@ -1,5 +1,17 @@
 # In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+ variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "S3 bucket name for static website"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
+  default     = "dev"
 }
